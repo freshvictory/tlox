f58fd8827bd4676b66eb73d049c9c20f920ea631
Justin Renjilian <justin.renjilian@gmail.com>
2020-08-23 12:09:56 -0400

    Cleanup, add Interpreter.elm.

--- src/web/public/lib/parser.js
+++ src/web/public/lib/parser.js
@@ -1,183 +1,181 @@
-"use strict";
 class ParseError extends Error {
 }
 class Parser {
-    constructor(tokens, error) {
-        this.current = 0;
-        this.tokens = tokens;
-        this.errorInternal = error;
+  constructor(tokens, error) {
+    this.current = 0;
+    this.tokens = tokens;
+    this.errorInternal = error;
+  }
+  parse() {
+    try {
+      return this.expression();
+    } catch (e) {
+      return null;
     }
-    parse() {
-        try {
-            return this.expression();
-        }
-        catch (e) {
-            return null;
-        }
-    }
-    match(...types) {
-        return types.some(t => {
-            if (this.check(t)) {
-                this.advance();
-                return true;
-            }
-            return false;
-        });
-    }
-    check(type) {
-        if (this.isAtEnd()) {
-            return false;
-        }
-        return this.peek().type === type;
-    }
-    advance() {
-        if (!this.isAtEnd()) {
-            this.current++;
-        }
-        return this.previous();
-    }
-    isAtEnd() {
-        return this.peek().type === 'EOF';
-    }
-    peek() {
-        return this.tokens[this.current];
-    }
-    previous() {
-        return this.tokens[this.current - 1];
-    }
-    consume(type, message) {
-        if (this.check(type)) {
-            return this.advance();
-        }
-        throw this.error(this.peek(), message);
-    }
-    error(token, message) {
-        this.errorInternal(token, message);
-        return new ParseError();
-    }
-    synchronize() {
+  }
+  match(...types) {
+    return types.some((t) => {
+      if (this.check(t)) {
         this.advance();
-        while (!this.isAtEnd()) {
-            if (this.previous().type === 'SEMICOLON') {
-                return;
-            }
-            switch (this.peek().type) {
-                case 'CLASS':
-                case 'FUN':
-                case 'VAR':
-                case 'FOR':
-                case 'IF':
-                case 'WHILE':
-                case 'PRINT':
-                case 'RETURN':
-                    return;
-            }
-            this.advance();
-        }
+        return true;
+      }
+      return false;
+    });
+  }
+  check(type) {
+    if (this.isAtEnd()) {
+      return false;
     }
-    expression() {
-        return this.equality();
+    return this.peek().type === type;
+  }
+  advance() {
+    if (!this.isAtEnd()) {
+      this.current++;
     }
-    equality() {
-        let expr = this.comparison();
-        while (this.match('BANG_EQUAL', 'EQUAL_EQUAL')) {
-            const operator = this.previous();
-            const right = this.comparison();
-            expr = {
-                type: 'binary',
-                left: expr,
-                operator,
-                right
-            };
-        }
-        return expr;
+    return this.previous();
+  }
+  isAtEnd() {
+    return this.peek().type === "EOF";
+  }
+  peek() {
+    return this.tokens[this.current];
+  }
+  previous() {
+    return this.tokens[this.current - 1];
+  }
+  consume(type, message) {
+    if (this.check(type)) {
+      return this.advance();
     }
-    comparison() {
-        let expr = this.addition();
-        while (this.match('GREATER', 'GREATER_EQUAL', 'LESS', 'LESS_EQUAL')) {
-            const operator = this.previous();
-            const right = this.addition();
-            expr = {
-                type: 'binary',
-                left: expr,
-                operator,
-                right
-            };
-        }
-        return expr;
+    throw this.error(this.peek(), message);
+  }
+  error(token, message) {
+    this.errorInternal(token, message);
+    return new ParseError();
+  }
+  synchronize() {
+    this.advance();
+    while (!this.isAtEnd()) {
+      if (this.previous().type === "SEMICOLON") {
+        return;
+      }
+      switch (this.peek().type) {
+        case "CLASS":
+        case "FUN":
+        case "VAR":
+        case "FOR":
+        case "IF":
+        case "WHILE":
+        case "PRINT":
+        case "RETURN":
+          return;
+      }
+      this.advance();
     }
-    addition() {
-        let expr = this.multiplication();
-        while (this.match('MINUS', 'PLUS')) {
-            const operator = this.previous();
-            const right = this.multiplication();
-            expr = {
-                type: 'binary',
-                left: expr,
-                operator,
-                right
-            };
-        }
-        return expr;
+  }
+  expression() {
+    return this.equality();
+  }
+  equality() {
+    let expr = this.comparison();
+    while (this.match("BANG_EQUAL", "EQUAL_EQUAL")) {
+      const operator = this.previous();
+      const right = this.comparison();
+      expr = {
+        type: "binary",
+        left: expr,
+        operator,
+        right
+      };
     }
-    multiplication() {
-        let expr = this.unary();
-        while (this.match('SLASH', 'STAR')) {
-            const operator = this.previous();
-            const right = this.unary();
-            expr = {
-                type: 'binary',
-                left: expr,
-                operator,
-                right
-            };
-        }
-        return expr;
+    return expr;
+  }
+  comparison() {
+    let expr = this.addition();
+    while (this.match("GREATER", "GREATER_EQUAL", "LESS", "LESS_EQUAL")) {
+      const operator = this.previous();
+      const right = this.addition();
+      expr = {
+        type: "binary",
+        left: expr,
+        operator,
+        right
+      };
     }
-    unary() {
-        if (this.match('BANG', 'MINUS')) {
-            const operator = this.previous();
-            const right = this.unary();
-            return {
-                type: 'unary',
-                operator,
-                right
-            };
-        }
-        return this.primary();
+    return expr;
+  }
+  addition() {
+    let expr = this.multiplication();
+    while (this.match("MINUS", "PLUS")) {
+      const operator = this.previous();
+      const right = this.multiplication();
+      expr = {
+        type: "binary",
+        left: expr,
+        operator,
+        right
+      };
     }
-    primary() {
-        if (this.match('FALSE')) {
-            return { type: 'literal', value: false, token: this.previous() };
-        }
-        if (this.match('TRUE')) {
-            return { type: 'literal', value: true, token: this.previous() };
-        }
-        if (this.match('NIL')) {
-            return { type: 'literal', value: null, token: this.previous() };
-        }
-        if (this.match('NUMBER', 'STRING')) {
-            return {
-                type: 'literal',
-                value: this.previous().literal,
-                token: this.previous()
-            };
-        }
-        if (this.match('LEFT_PAREN')) {
-            const firstParen = this.previous();
-            const expr = this.expression();
-            this.consume('RIGHT_PAREN', `Expect ')' after expression.`);
-            return {
-                type: 'grouping',
-                expression: expr,
-                tokens: [firstParen, this.previous()]
-            };
-        }
-        throw this.error(this.peek(), "Expect expression.");
+    return expr;
+  }
+  multiplication() {
+    let expr = this.unary();
+    while (this.match("SLASH", "STAR")) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = {
+        type: "binary",
+        left: expr,
+        operator,
+        right
+      };
     }
+    return expr;
+  }
+  unary() {
+    if (this.match("BANG", "MINUS")) {
+      const operator = this.previous();
+      const right = this.unary();
+      return {
+        type: "unary",
+        operator,
+        right
+      };
+    }
+    return this.primary();
+  }
+  primary() {
+    if (this.match("FALSE")) {
+      return {type: "literal", value: false, token: this.previous()};
+    }
+    if (this.match("TRUE")) {
+      return {type: "literal", value: true, token: this.previous()};
+    }
+    if (this.match("NIL")) {
+      return {type: "literal", value: null, token: this.previous()};
+    }
+    if (this.match("NUMBER", "STRING")) {
+      return {
+        type: "literal",
+        value: this.previous().literal,
+        token: this.previous()
+      };
+    }
+    if (this.match("LEFT_PAREN")) {
+      const firstParen = this.previous();
+      const expr = this.expression();
+      this.consume("RIGHT_PAREN", `Expect ')' after expression.`);
+      return {
+        type: "grouping",
+        expression: expr,
+        tokens: [firstParen, this.previous()]
+      };
+    }
+    throw this.error(this.peek(), "Expect expression.");
+  }
 }
 function parse(tokens, error) {
-    return new Parser(tokens.filter(t => {
-        return t.type !== 'WHITESPACE' && t.type !== 'COMMENT';
-    }), error).parse();
+  return new Parser(tokens.filter((t) => {
+    return t.type !== "WHITESPACE" && t.type !== "COMMENT";
+  }), error).parse();
 }
--- src/web/src/Main.elm
+++ src/web/src/Main.elm
@@ -12,6 +12,7 @@ import List
 import Theme exposing (Theme, light, dark)
 import Css.Media
 import Css.Global
+import Interpreter exposing (..)
 
 
 
@@ -188,7 +189,6 @@ update msg model =
                         Ok err ->
                             ( { model
                               | parseErrors = model.parseErrors ++ [ err ]
-                              , runResult = Nothing
                               }
                             , Cmd.none
                             )
@@ -496,6 +496,7 @@ viewTabRadio model id label tab =
             [ Css.lineHeight (Css.num 1)
             , Css.padding (rem 0.5)
             , Css.border3 (px 1) Css.solid (hex light.purple)
+            , Css.borderRight Css.zero
             , Css.display Css.block
             , Css.firstChild
                 [ Css.borderTopLeftRadius (rem 0.25)
@@ -504,6 +505,7 @@ viewTabRadio model id label tab =
             , Css.lastChild
                 [ Css.borderTopRightRadius (rem 0.25)
                 , Css.borderBottomRightRadius (rem 0.25)
+                , Css.borderRight3 (px 1) Css.solid (hex light.purple)
                 ]
             , if model.tab == tab then
                 Css.batch
@@ -739,7 +741,7 @@ viewExpression model expr =
                     Just e ->
                         ( if expr == e then
                             [ Css.Global.descendants
-                                [ Css.Global.typeSelector "button"
+                                [ Css.Global.button
                                     [ themed
                                         [ (Css.backgroundColor, .highlight) ]
                                     ]
@@ -895,349 +897,6 @@ viewError model =
                         errors
                     )
                 ]
-
-
-
----- SCANNER ----
-
-
-type alias ScanError =
-    { line : Int
-    , message : String
-    }
-
-
-type alias ParseError =
-    { token : Token
-    , message : String
-    }
-
-
-type Expr
-    = Binary BinaryExpr
-    | Unary UnaryExpr
-    | Grouping GroupingExpr
-    | Literal LiteralExpr
-
-
-type alias BinaryExpr =
-  { left: Expr
-  , right: Expr
-  , operator: Token
-  }
-
-
-type alias UnaryExpr =
-    { operator: Token
-    , right: Expr
-    }
-
-
-type alias GroupingExpr =
-    { expression: Expr
-    , tokens: List Token
-    }
-
-
-type alias LiteralExpr =
-    { value: TokenLiteral
-    , token: Token
-    }
-
-
-
-type alias Token =
-    { tokenType : TokenType
-    , tokenTypeString : String
-    , lexeme : String
-    , line : Int
-    , start : Int
-    , literal : Maybe TokenLiteral
-    }
-
-
-type TokenType
-    = LEFT_PAREN | RIGHT_PAREN
-    | LEFT_BRACE | RIGHT_BRACE
-    | COMMA | DOT | MINUS | PLUS | SEMICOLON | SLASH | STAR
-    | BANG | BANG_EQUAL | EQUAL | EQUAL_EQUAL 
-    | GREATER | GREATER_EQUAL | LESS | LESS_EQUAL
-    | IDENTIFIER | STRING | NUMBER
-    | AND | CLASS | ELSE | FALSE | FUN | FOR
-    | IF | NIL | OR | PRINT | RETURN | SUPER
-    | THIS | TRUE | VAR | WHILE
-    | COMMENT | WHITESPACE | UNEXPECTED | EOF
-
-
-type TokenLiteral
-    = Str String
-    | Num Float
-    | Boolean Bool
-    | Nil
-
-
-decodeExpr : Decoder Expr
-decodeExpr =
-    field "type" Json.Decode.string
-        |> Json.Decode.andThen decodeExprType
-
-
-decodeExprType : String -> Decoder Expr
-decodeExprType s =
-    case s of
-        "binary" ->
-            Json.Decode.map Binary
-                ( Json.Decode.map3 BinaryExpr
-                    (field "left" decodeExpr)
-                    (field "right" decodeExpr)
-                    (field "operator" decodeToken)
-                )
-
-        "unary" ->
-            Json.Decode.map Unary
-                ( Json.Decode.map2 UnaryExpr
-                    (field "operator" decodeToken)
-                    (field "right" decodeExpr)
-                )
-    
-        "grouping" ->
-            Json.Decode.map Grouping
-                ( Json.Decode.map2 GroupingExpr
-                    (field "expression" decodeExpr)
-                    (field "tokens" (Json.Decode.list decodeToken))
-                )
-
-        "literal" ->
-            Json.Decode.map Literal
-                ( Json.Decode.map2 LiteralExpr
-                    (field "value" decodeTokenLiteral)
-                    (field "token" decodeToken)
-                )
-
-        _ ->
-            Json.Decode.fail ("Unknown expr type: " ++ s)
-
-
-encodeExpr : Expr -> Json.Decode.Value
-encodeExpr e =
-    case e of
-        Binary b ->
-            Json.Encode.object
-                ( [ ( "type", Json.Encode.string "binary" )
-                  , ( "left", encodeExpr b.left)
-                  , ( "right", encodeExpr b.right)
-                  , ( "operator", encodeToken b.operator)
-                  ]
-                )
-
-        Unary u ->
-            Json.Encode.object
-                ( [ ( "type", Json.Encode.string "unary" )
-                  , ( "right", encodeExpr u.right)
-                  , ( "operator", encodeToken u.operator)
-                  ]
-                )
-
-        Grouping g ->
-            Json.Encode.object
-                ( [ ( "type", Json.Encode.string "grouping" )
-                  , ( "expression", encodeExpr g.expression )
-                  , ( "tokens", Json.Encode.list encodeToken g.tokens)
-                  ]
-                )
-
-        Literal l ->
-            Json.Encode.object
-                ( [ ( "type", Json.Encode.string "literal" )
-                  , ( "value"
-                    , encodeTokenLiteral
-                        ( Maybe.withDefault
-                          Nil
-                          l.token.literal
-                        )
-                    )
-                  ]
-                )
-
-
-decodeParseError : Decoder ParseError
-decodeParseError =
-    Json.Decode.map2 ParseError
-        (field "token" decodeToken)
-        (field "message" Json.Decode.string)
-
-
-encodeToken : Token -> Json.Decode.Value
-encodeToken token =
-    Json.Encode.object
-        ( [ ( "type", Json.Encode.string token.tokenTypeString )
-          , ( "lexeme", Json.Encode.string token.lexeme )
-          , ( "line", Json.Encode.int token.line )
-          , ( "start", Json.Encode.int token.start )
-          ]
-        ++ case token.literal of
-            Nothing -> []
-            Just v ->
-                [ ( "literal", encodeTokenLiteral v ) ]
-        )
-
-
-encodeTokenLiteral : TokenLiteral -> Json.Decode.Value
-encodeTokenLiteral l =
-    case l of
-        Str s -> Json.Encode.string s
-        Num f -> Json.Encode.float f
-        Boolean b -> Json.Encode.bool b
-        Nil -> Json.Encode.null
-
-
-decodeToken : Decoder Token
-decodeToken =
-    Json.Decode.map6 Token
-        (field "type" decodeTokenType)
-        (field "type" Json.Decode.string)
-        (field "lexeme" Json.Decode.string)
-        (field "line" Json.Decode.int)
-        (field "start" Json.Decode.int)
-        (Json.Decode.maybe (field "literal" decodeTokenLiteral))
-
-
-decodeTokenType : Decoder TokenType
-decodeTokenType =
-    Json.Decode.string
-        |> Json.Decode.andThen
-            ( \s ->
-                case s of
-                    "LEFT_PAREN" -> Json.Decode.succeed LEFT_PAREN
-                    "RIGHT_PAREN" -> Json.Decode.succeed RIGHT_PAREN
-                    "LEFT_BRACE" -> Json.Decode.succeed LEFT_BRACE
-                    "RIGHT_BRACE" -> Json.Decode.succeed RIGHT_BRACE
-                    "COMMA" -> Json.Decode.succeed COMMA
-                    "DOT" -> Json.Decode.succeed DOT
-                    "MINUS" -> Json.Decode.succeed MINUS
-                    "PLUS" -> Json.Decode.succeed PLUS
-                    "SEMICOLON" -> Json.Decode.succeed SEMICOLON
-                    "SLASH" -> Json.Decode.succeed SLASH
-                    "STAR" -> Json.Decode.succeed STAR
-                    "BANG" -> Json.Decode.succeed BANG
-                    "BANG_EQUAL" -> Json.Decode.succeed BANG_EQUAL
-                    "EQUAL" -> Json.Decode.succeed EQUAL
-                    "EQUAL_EQUAL" -> Json.Decode.succeed EQUAL_EQUAL
-                    "GREATER" -> Json.Decode.succeed GREATER
-                    "GREATER_EQUAL" -> Json.Decode.succeed GREATER_EQUAL
-                    "LESS" -> Json.Decode.succeed LESS
-                    "LESS_EQUAL" -> Json.Decode.succeed LESS_EQUAL
-                    "IDENTIFIER" -> Json.Decode.succeed IDENTIFIER
-                    "STRING" -> Json.Decode.succeed STRING
-                    "NUMBER" -> Json.Decode.succeed NUMBER
-                    "AND" -> Json.Decode.succeed AND
-                    "CLASS" -> Json.Decode.succeed CLASS
-                    "ELSE" -> Json.Decode.succeed ELSE
-                    "FALSE" -> Json.Decode.succeed FALSE
-                    "FUN" -> Json.Decode.succeed FUN
-                    "FOR" -> Json.Decode.succeed FOR
-                    "IF" -> Json.Decode.succeed IF
-                    "NIL" -> Json.Decode.succeed NIL
-                    "OR" -> Json.Decode.succeed OR
-                    "PRINT" -> Json.Decode.succeed PRINT
-                    "RETURN" -> Json.Decode.succeed RETURN
-                    "SUPER" -> Json.Decode.succeed SUPER
-                    "THIS" -> Json.Decode.succeed THIS
-                    "TRUE" -> Json.Decode.succeed TRUE
-                    "VAR" -> Json.Decode.succeed VAR
-                    "WHILE" -> Json.Decode.succeed WHILE
-                    "COMMENT" -> Json.Decode.succeed COMMENT
-                    "WHITESPACE" -> Json.Decode.succeed WHITESPACE
-                    "UNEXPECTED" -> Json.Decode.succeed UNEXPECTED
-                    "EOF" -> Json.Decode.succeed EOF
-                    _ -> Json.Decode.fail ("Unexpected token " ++ s)
-            )
-
-
-decodeTokenLiteral : Decoder TokenLiteral
-decodeTokenLiteral =
-    Json.Decode.oneOf
-        [ Json.Decode.map Str Json.Decode.string
-        , Json.Decode.map Num Json.Decode.float
-        , Json.Decode.map Boolean Json.Decode.bool
-        , Json.Decode.null Nil
-        ]
-
-
-tokenLiteralString : TokenLiteral -> String
-tokenLiteralString l =
-    case l of
-        Str s -> s
-        Num f -> String.fromFloat f
-        Boolean b -> if b then "true" else "false"
-        Nil -> "null"
-
-
-decodeRunResult : Decoder TokenLiteral
-decodeRunResult =
-    field "result" decodeTokenLiteral
-
-
-exprToken : Expr -> List Token
-exprToken expr =
-    case expr of
-        Binary e ->
-            [e.operator]
-        
-        Unary e ->
-            [e.operator]
-
-        Grouping e ->
-            e.tokens
-
-        Literal e ->
-            [e.token]
-
-
-getExprTokenMin : Expr -> Maybe Int
-getExprTokenMin expr =
-    case expr of
-        Binary e ->
-            getExprTokenMin e.left
-
-        Unary e ->
-            Just e.operator.start
-
-        Grouping e ->
-            Maybe.map .start (List.head e.tokens)
-
-        Literal e ->
-            Just e.token.start
-
-
-getExprTokenMax : Expr -> Maybe Int
-getExprTokenMax expr =
-    case expr of
-        Binary e ->
-            getExprTokenMax e.right
-
-        Unary e ->
-            getExprTokenMax e.right
-
-        Grouping e ->
-            Maybe.map .start (List.head (List.reverse e.tokens))
-
-        Literal e ->
-            Just e.token.start
-
-
-getExprTokenRange : List Token -> Expr -> List Token
-getExprTokenRange full expr =
-    let
-        (min, max) = (getExprTokenMin expr, getExprTokenMax expr)
-    in
-        List.filter
-            ( \t ->
-                t.start >= Maybe.withDefault 0 min
-                && t.start <= Maybe.withDefault t.start max
-            )
-            full
-
 
 
 
